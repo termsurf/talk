d398ea391a97ec37c32285c7d9dcb023ef9b8d79
Lance Pollard <lp@elk.fm>
2021-07-01 17:34:09 -0700

    update

--- test.js
+++ test.js
@@ -8,11 +8,11 @@ match('HaarijE', 'ḥaarıjẹ')
 match('eTemu', 'eṭemu')
 match('txya^a_a^u', 'txyáàáu')
 match('hwpo~kUimUno~s', 'hwpo̤kụımụno̤s')
-match('sinh(o)r!Esi', 'sınhȯŕẹsı')
+match('sinh(o)rEsi', 'sınhȯrẹsı')
 match("batoo'aH", "batoo'aḥ")
 match('aiyur~aK', 'aıyur̤aḳ')
 match("s'or~ya&te", "s'or̤ya̰te")
-match('t!ar!Eba', 't̖aŕẹba')
+match('t!arEba', 't̖arẹba')
 match('txhaK!EnEba', 'txhaḳ̖ẹnẹba')
 
 function match(a, b) {
