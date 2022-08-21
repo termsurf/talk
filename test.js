c979f60e4f0ce8d66b4992f2d9385b6b43899eba
Lance Pollard <lp@elk.fm>
2022-08-21 00:41:09 -0700

    commit old stuff

--- test.js
+++ test.js
@@ -2,13 +2,13 @@
 const assert = require('assert')
 const form = require('.')
 
-match('txaand(o)', 'txaandȯ')
-match('surdjy(o)', 'surdjyȯ')
-match('HaarijE', 'ḥaarıjẹ')
-match('eTemu', 'eṭemu')
-match('txya+a-a+u', 'txyáàáu')
+match('txaando^', 'txaandȯ')
+match('surdjyo^', 'surdjyȯ')
+match('HaarijE', 'ḥaarıjẹ')
+match('eTemu', 'eṭemu')
+match('txya+a-a+u', 'txyàáàu')
 match('hwpo#kUimUno#s', 'hwpo̤kụımụno̤s')
-match('sinh(o)rEsi', 'sınhȯrẹsı')
+match('sinho^rEsi', 'sınhȯrẹsı')
 match("batoo'aH", "batoo'aḥ")
 match('aiyuQaK', 'aıyuq̇aḳ')
 match("s'oQya~te", "s'oq̇ya̰te")
@@ -16,5 +16,10 @@ match('t!arEba', 't̖arẹba')
 match('txhaK!EnEba', 'txhaḳ̖ẹnẹba')
 
 function match(a, b) {
-  assert.strictEqual(form(a), b)
+  const o = form(a)
+  o.split('').forEach((x, i) => {
+    if (x !== b[i]) {
+      throw new Error(`${o} with ${x} != ${b[i]}`)
+    }
+  })
 }
